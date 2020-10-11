7b87e6663678ce767007bad139a49fd1410fdfb3
Chris <brotherchris65@gmail.com>
2020-10-11 17:04:56 -0500

    initial

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name?', validators=[DataRequired()])
+    submit=SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "10012019willliveininfamy"
 
 bootstrap = Bootstrap(app)
 
@@ -14,8 +22,13 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+        name = None
+        form = NameForm()
+        if form.validate_on_submit():
+            name=form.name.data
+            form.name.data = ''
         return render_template('index.html')
 
 @app.route('/user/<name>')
