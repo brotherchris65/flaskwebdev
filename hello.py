cce85b85aabf9d8295a303ab7d09622f2d035c32
chris <brotherchris65@gmail.com>
2020-10-19 03:34:08 -0500

    set up db

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class NameForm(FlaskForm):
     name=StringField('What is your name?', validators=[DataRequired()])
@@ -10,6 +14,30 @@ class NameForm(FlaskForm):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "10012019willliveininfamy"
+app.config['SQLAlchemy_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db= SQLAlchemy(app)
+
+"""Set up the Database here"""
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users=db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return'<Role %r>' % self.name
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return'<User %r>' % self.username
+
 
 bootstrap = Bootstrap(app)
 
